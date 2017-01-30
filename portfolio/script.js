741de1a0f9c29e06b96ba2b7778999f724c20781
Bartlomiej Laskowski <frameture@gmail.com>
2017-01-30 11:49:17 +0100

    Navbar buttons scroll to its content

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+function start() {
+  $(".btn-about").click(function(e) {
+    window.scrollTo(0, 0);
+  });
+  $(".btn-portfolio").click(function(e) {
+    window.scrollTo(0, 360);
+  });
+  $(".btn-contact").click(function(e) {
+    window.scrollTo(0, 1000);
+  });
+}
