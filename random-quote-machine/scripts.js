03da10440c2a555751e81f265c1a6ca824eb8395
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-01 19:28:04 +0100

    Colors animate when new quote generated

--- random-quote-machine/scripts.js
+++ random-quote-machine/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-  setQuote();
-  onResize();
-  $(".button").on("click", function() {
+  $(".new-quote").on("click", function() {
+    animateIt();
     setQuote();
     onResize();
   });
+
+  $(".twitter-share-button").on("click", function tweet(e) {
+    $(".twitter-share-button").attr('href', 
+    "https://twitter.com/intent/tweet?text=" 
+    + encodeURI($(".quote").text()) + "-  " + encodeURI($(".quote-author").text()));
+  });
+  $(".new-quote").click(); // Initialize new quote generating;
 });
+
+function animateIt() {
+  var colors = getColors()[Math.floor(Math.random() * 6)];
+  last = "#000";
+  while (last == colors[0])
+    colors = getColors()[Math.floor(Math.random() * 6)];
+  last = colors[0];
+  console.log(colors[0], last);
+  
+  $("body").animate( { backgroundColor: colors[0] }, 400);
+  $(".quote-container").animate( { backgroundColor: colors[1], 
+    borderColor: colors[1], color: colors[0]}, 400);
+  $(".button").animate( { backgroundColor: colors[0], color: colors[1] }, 400);
+}
 
 function onResize() {
   $(window).resize(function() {
@@ -16,24 +36,29 @@ function onResize() {
     });
   });
   $(window).resize();
-  $(window).resize();
 }
 
 function setQuote() {
   var quote = newQuote();
-  $(".quote").text(quote["quote"]);
+  $(".quote").html("<i class='fa fa-quote-left'></i>  " +  quote["quote"] 
+      + "  <i class='fa fa-quote-right'></i>");
   $(".quote-author").text(quote["author"]);
 }
 
 function newQuote() {
   var QUOTES = getQuotes();
-  var last, random;
+  var last = 0, random = 0;
 
   while (last == random)
-   random = Math.floor((Math.random() * 6) + 1);
+   random = Math.floor((Math.random() * 12) + 1);
   last = random;
 
   return QUOTES[random];
+}
+
+function getColors() {
+return [["#333", "#fff"], ["#854785", "#B6C669"], ["#43787D", "#CD9A6D"], 
+        ["#A5C166", "#A25684"], ["#57A457", "#CD6D6D"], ["#734C89", "#CDCD6D"]]
 }
 
 function getQuotes() {
@@ -61,6 +86,30 @@ function getQuotes() {
     "6": {
       "author": "Henry Ford",
       "quote": "My best friend is the one who brings out the best in me."
+    },
+    "7": {
+      "author": "George Allen, Sr.",
+      "quote": "Work hard, stay positive, and get up early. It's the best part of the day."
+    },
+    "8": {
+      "author": "Benjamin Franklin",
+      "quote": "An investment in knowledge pays the best interest."
+    },
+    "9": {
+      "author": "Buddha",
+      "quote": "Health is the greatest gift, contentment the greatest wealth, faithfulness the best relationship."
+    },
+    "10": {
+      "author": "Mahatma Gandhi",
+      "quote": "The best way to find yourself is to lose yourself in the service of others."
+    },
+    "11": {
+      "author": "Dave Pelzer",
+      "quote": "To help yourself, you must be yourself. Be the best that you can be. When you make a mistake, learn from it, pick yourself up and move on."
+    },
+    "12": {
+      "author": "Theodore Roosevelt",
+      "quote": "Keep your eyes on the stars, and your feet on the ground."
     }
   }
 }
