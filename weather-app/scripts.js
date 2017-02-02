0b2d6edc6d58619e656ba2aed7a202991427bb4a
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-02 14:51:42 +0100

    weather app - initial commit

---
+++ weather-app/scripts.js
@@ -0,0 +1,9 @@
+function getIcon(name) {
+  var ICONS = {
+    'sun': 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-clear-icon.png',
+    'rain': 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-showers-icon.png',
+    'cloudy': 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-many-clouds-icon.png',
+    'partly-cloudy': 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/128/Status-weather-clouds-icon.png',
+    'windy': 'http://icons.iconarchive.com/icons/custom-icon-design/lovely-weather/128/wind-night-icon.png'
+  }
+}
