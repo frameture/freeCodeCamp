5d8c3a916468b5e075bfb5fcfce2e65e90fab0e0
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-12 21:02:48 +0100

    vs Player mode adds signs to the grid

--- tictactoe/scripts.js
+++ tictactoe/scripts.js
@@ -2,47 +2,61 @@ var app = getApp();
 $(document).ready(app.main);
 
 function getApp() {
+  // Private members
   var MODES = ['pl-one', 'pl-two'];
   var SIGNS = ['x', 'o'];
   var score = {
     'pl-one': 0,
     'pl-two': 0
   };
-  var sign = {
-    'pl-one': null,
-    'pl-two': null
-  };
+  var sign = [];
   var mode;
   var turn;
+  var running = false;
+
   var app = {};
-  app.main = main;
+  app.main = main; // Public member.
 
   function main() {
     registerModesHandler();
     registerBackButton();
     registerSignPick();
     registerResetHandler();
+    registerGridHandler();
   }
 
   // Inner functions.
+
+  function registerGridHandler() {
+    $('.cell').on('click', cellClickHandler);
+  }
+
+  function cellClickHandler(ele) {
+    ele = ele.delegateTarget;
+    var cN = ele.className;
+    if (cN.includes('x-sign') || cN.includes('o-sign'))
+      return;
+    $(ele).addClass(sign[turn] + '-sign');
+    turn = turn === 0 ? 1 : 0;
+    showTurn();
+  }
 
   function registerResetHandler() {
     $('.reset').on('click', resetHandler);
   }
 
   function resetHandler() {
+    running = false;
     score = {
       'pl-one': null,
       'pl-two': null
     };
-    sign = {
-    'pl-one': null,
-    'pl-two': null
-    };
+    sign = [];
     turn = null;
     mode = null;
     hideShow('.grid', '.intro');
     hideShow('.score-keeper');
+    $('.cell').removeClass('x-sign o-sign');
   }
 
   function registerSignPick() {
@@ -52,11 +66,11 @@ function getApp() {
   function signHandler(ele) {
     var id = ele.delegateTarget.id;
     if (id === 'x') {
-      sign['pl-one'] = SIGNS[0];
-      sign['pl-two'] = SIGNS[1];
+      sign[0] = SIGNS[0];
+      sign[1] = SIGNS[1];
     } else {
-      sign['pl-one'] = SIGNS[1];
-      sign['pl-two'] = SIGNS[0];
+      sign[0] = SIGNS[1];
+      sign[1] = SIGNS[0];
     }
 
     hideShow('.sign-pick', '.grid');
@@ -95,6 +109,8 @@ function getApp() {
   }
 
   function startGame() {
+    turn = Math.random() > .49 ? 0 : 1;
+    running = true;
     if (mode === MODES[0])
       vsComputer();
     else
@@ -102,12 +118,33 @@ function getApp() {
   }
 
   function vsComputer() {
-    // TODO
+    $('.player-two-name').text('computer');
+    $('.player-one-turn').text("Your turn");
+    $('.player-two-turn').text("Computer's turn");
+    showTurn();
   }
 
   function vsPlayer() {
-    // TODO
+    $('.player-two-name').text('player II');
+    $('.player-one-turn').text("First player turn");
+    $('.player-two-turn').text("Second player turn");
+    showTurn();
   }
-
+  function showTurn() {
+    var $toHide, $toShow;
+    if (turn === 0) {
+      $toHide = $('.player-two-turn');
+      $toShow = $('.player-one-turn');
+    } else {
+      $toShow = $('.player-two-turn');
+      $toHide = $('.player-one-turn');
+    }
+    if ($toHide.css('display') !== 'none') {
+      $toHide.animate( { opacity: 0 }, 500 );
+      $toHide.css('display', 'none');
+    }
+    $toShow.css('display', 'inline');
+    $toShow.animate( { opacity: 1 }, 500 );
+  }
   return app;
 }
