b1852f05a237c7921f938993b9c66255896e7b00
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-14 21:40:46 +0100

    SVG colored components

---
+++ simon-game/scripts.js
@@ -0,0 +1,16 @@
+var app = getApp();
+
+$(document).ready(app.main);
+
+function getApp() {
+  var app = {};
+  app.main = main;
+
+  // Other private members.
+
+  function main() {
+   // TODO
+  } 
+
+  return app; 
+}
