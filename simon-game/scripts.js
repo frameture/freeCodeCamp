1a5d510033415bac946638bc3f75a7fa33aff695
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-15 11:43:41 +0100

    structure of scripts

--- simon-game/scripts.js
+++ simon-game/scripts.js
@@ -7,10 +7,54 @@ function getApp() {
   app.main = main;
 
   // Other private members.
+  var power = false;
+  var started = false;
+  var strict = false;
+  var levels = [];
+  // count = levels.length;
 
   function main() {
-   // TODO
+   registerControlsHandlers();
+   registerQuartersHandler();
+  }
+
+  function registerControlsHandlers() {
+    $('.switch-container').on('click', powerSwitch);
+    $('.start-display').on('click', startSwitch);
+    $('.strict-display').on('click', strictSwitch);
+
+    // Inner functions
+    function powerSwitch() {
+      power = ! power;
+      // TODO
+    }
+
+    function startSwitch() {
+      if (! power)
+        return;
+      start = ! start;
+      // TODO
+    }
+
+    function strictSwitch() {
+      if (! power)
+        return;
+      strict = ! strict;
+      // TODO
+    }
   } 
+
+  function registerQuartersHandler() {
+    $('.path').on('click', quarterClick);
+
+    // Inner functions
+    function quarterClick() {
+      if (! power || ! start)
+        return;
+      // TODO
+    }
+    
+  }
 
   return app; 
 }
