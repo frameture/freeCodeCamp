a1339fbb81fbbd74d3318e4b8e51251c44bd5150
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-15 22:50:43 +0100

    logic for checking order and correctness of clicks

--- simon-game/scripts.js
+++ simon-game/scripts.js
@@ -11,8 +11,8 @@ function getApp() {
   var started = false;
   var strict = false;
   var levels = [];
+  var clicked = [];
   var clickable = false;
-  // count = levels.length;
 
   function main() {
    registerControlsHandlers();
@@ -30,6 +30,9 @@ function getApp() {
       power = ! power;
       started = false;
       strict = false;
+      levels = [];
+      clicked = [];
+      clickable = false;
 
       endGame();
       updateDisplay('- -');
@@ -54,10 +57,9 @@ function getApp() {
         return;
 
       if (started)
-        resetGame();
-      
-      started = true;
-      startGame();
+        resetGame('- -', true, true);
+      else
+        startGame('- -', true);
     }
 
     function strictSwitch() {
@@ -75,85 +77,130 @@ function getApp() {
   }
 
   function updateDisplay(value) {
-    console.log(value);
     $('.count-value').text(value);
   }
 
-  function resetGame() {
-    // reset current game
-    console.log('resetting current game');
-    endGame();
-    startGame();
+  function resetGame(text, deepReset, next) {
+    endGame(deepReset);
+    startGame(text, next);
   }
 
-  function endGame() {
+  function endGame(deepReset) {
     clickable = false;
-    console.log('ending current game');    
+    clicked = [];
+
+    if (deepReset) {
+      levels = [];
+    }
   }
 
-  function startGame() {
-    // TODO
-    console.log('starting current game');    
-    
-    levels = [];
-    animateDisplay();
-    setTimeout(addNextLevel, 1500);
+  function startGame(text, next) {
+    started = true;
+    animateDisplay(text);
+    if (next)
+      setTimeout(addNextLevel, 1500);
     setTimeout(showLevels, 1700);
+  }
 
-    // Inner functions
-    function animateDisplay() {
-      var times = 2;
+  function animateDisplay(text) {
+    var times = 2;
+    animate();
 
-      animate();
-
-      function animate() {
-        if (!times)
-          return;
-        times--; 
-        updateDisplay('');
-        setTimeout(function() {
-          updateDisplay('- -');
-          setTimeout(animate, 300);
-        }, 300);
-      }
-    }
-
-    function showLevels() {
-      levels.forEach(function(lvl) {
-        showLvl(lvl);
-      });
-      updateDisplay(levels.length);
-      clickable = true;
-
-      // Inner function
-      function showLvl(lvl) {
-        changeColor(lvl, 1);
-        playSound(lvl, 1);
-        setTimeout(function() {
-          changeColor(lvl, 0);
-          playSound(lvl, 0);
-        }, 500);
-      }
-    }
-
-    function addNextLevel() {      
-      var nextLevel = Math.ceil((Math.random() * 4));
-      levels.push(nextLevel);
+    function animate() {
+      if (!times)
+        return;
+      times--; 
+      updateDisplay('');
+      setTimeout(function() {
+        updateDisplay(text);
+        setTimeout(animate, 300);
+      }, 300);
     }
   } 
 
+  function showLevels() {
+    var offset;
+    levels.forEach(function(lvl, i) {
+      offset = i;
+      setTimeout(showQuarter, i * 1000, lvl);
+    });
+    updateDisplay(levels.length);
+    setTimeout(function() {
+      clickable = true;
+    }, offset * 1000);
+    clicked = [];
+  }
+
+  function showQuarter(id) {
+    changeColor(id, 0);
+    playSound(id);
+    setTimeout(changeColor, 500, id, 1);
+
+    //Inner functions
+    function changeColor(id, saturation) {
+      var COLORS = [
+        ['rgb(0, 250, 0)', 'rgb(50, 170, 50)'],
+        ['red', 'rgb(170, 50, 50)'],
+        ['blue', 'rgb(50, 50, 170)'],
+        ['yellow', 'rgb(170, 170, 50)']
+      ];
+      $('#' + id).css('fill', COLORS[id][saturation]);
+    }
+
+    function playSound(id, play) {
+      var SOUNDS = [ 
+        './sounds/simonSound1.mp3', 
+        './sounds/simonSound2.mp3', 
+        './sounds/simonSound3.mp3', 
+        './sounds/simonSound4.mp3'
+      ]
+
+      new Audio(SOUNDS[id]).play();
+    } 
+  }
+
+
+  function addNextLevel() {      
+    var nextLevel = Math.floor((Math.random() * 4));
+    levels.push(nextLevel);
+  }
+
   function registerQuartersHandler() {
-    $('.path').on('click', quarterClick);
+    $('path').on('click', quarterClick);
 
     // Inner functions
-    function quarterClick() {
+    function quarterClick(e) {
       if (! power || ! started || ! clickable)
         return;
-      // TODO
 
-      // consider strict
+      clicked.push(e.delegateTarget.id);
+      showQuarter(e.delegateTarget.id);
+      checkClickedOrder();
+    }
 
-      // clickable = false;
+    function checkClickedOrder() {
+      // Check if failed
+      for (var i = 0; i < clicked.length; i++) {
+        if (clicked[i] != levels[i]) {
+          clickable = false;
+          clicked = [];
+          if (strict)
+            resetGame('! !', true, true);
+          else
+            resetGame('! !', false);
+          return;
+        }
+      }
+
+      if (clicked.length === levels.length) {
+        clickable = false;
+        if (levels.length === 20) {
+          resetGame('*WIN*', true, true);
+        } else {
+          setTimeout(addNextLevel, 1500);
+          setTimeout(showLevels, 1700);
+        }
+      }
     }
 
   }
