ba9bc61397f67c0b589a1d9ccdf99bceb89f335c
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-27 16:03:54 +0100

    updating root module

--- markdown-previewer/src/app/app.module.ts
+++ markdown-previewer/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }       from './app.component';
+import { MarkdownComponent }  from './markdown.component';
+import { PreviewerComponent } from './previewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarkdownComponent,
+    PreviewerComponent
   ],
   imports: [
     BrowserModule,
