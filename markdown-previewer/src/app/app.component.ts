cda9668edabc51232ff4d016759b4a7890144dae
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-27 20:02:12 +0100

    parsing markdown

--- markdown-previewer/src/app/app.component.ts
+++ markdown-previewer/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Output, OnInit, Component}   from '@angular/core';
 
-import { MarkdownComponent }  from './markdown.component';
-import { PreviewerComponent } from './previewer.component';
+import { MARKDOWN } from './markdown';
+
+var marked = require('marked'); // simple JS 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './views/app.component.html',
-  styleUrls: [ './styles/app.component.css']
+  templateUrl: './views/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  markdown: string;
+  private previewer: string; 
+
+  private getMarkdown(): void {
+    this.markdown = MARKDOWN;
+  }
+
+  private parseMarkdown(): void {
+    this.previewer = marked(this.markdown);
+  }
+
+   ngOnInit(): void {
+    this.getMarkdown();
+    this.parseMarkdown(); 
+  }
+}
--- markdown-previewer/src/app/app.module.ts
+++ markdown-previewer/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule }      from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
-import { AppComponent }       from './app.component';
-import { MarkdownComponent }  from './markdown.component';
-import { PreviewerComponent } from './previewer.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MarkdownComponent,
-    PreviewerComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
