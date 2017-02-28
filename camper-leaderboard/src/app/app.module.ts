3dbe73f3ab0ca5cf65077e09aa0c994894d3b383
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-28 11:10:23 +0100

    providing campers-service

--- camper-leaderboard/src/app/app.module.ts
+++ camper-leaderboard/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { CampersService } from './campers.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [CampersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
