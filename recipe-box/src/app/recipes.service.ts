cd842fd19a603384e6829744f3d0a1abed416057
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-28 22:18:03 +0100

    mock recipes-service

---
+++ recipe-box/src/app/recipes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core'
+
+import { Recipe } from './recipe';
+
+@Injectable()
+export class RecipesService {
+   private mockRecipes: Recipe[] = [
+    new Recipe('Apple Pie', ['apples', 'flour', 'water', 'sugar']),
+    new Recipe('Oats',      ['oats', 'water', 'honey', 'oil']),
+    new Recipe('Pasta',     ['flour', 'water'])
+  ];
+
+  public getRecipes(): Promise<Recipe[]> {
+    return Promise.resolve(this.mockRecipes);
+  }
+}
+
