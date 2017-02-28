1db4b67241542b9efb697743e02d20b1e6b4a9ba
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-28 22:18:51 +0100

    consume  recipies-service

--- recipe-box/src/app/app.component.ts
+++ recipe-box/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe }         from './recipe';
+import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './views/app.component.html',
-  styleUrls:  ['./styles/app.component.css']
+  styleUrls:  ['./styles/app.component.css'],
+  providers:  [ RecipesService ] 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private recipes: Recipe[];
+  private newRecipe = false;
+
+  constructor(private recipesService: RecipesService) { }
+
+  ngOnInit(): void {
+    this.getRecipes();
+  }
+
+  private getRecipes(): void {
+    this.recipesService.getRecipes()
+      .then(recipes => this.recipes = recipes);
+  }
+
+  private addRecipe(): void {
+    this.newRecipe = true;
+  }
+
+  private cancelRecipe(): void {
+    this.newRecipe = false;
+  }
+
 }
