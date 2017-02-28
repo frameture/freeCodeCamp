af976e8091a7280b53dfd8d909fa3857b0842cc3
Bartlomiej Laskowski <frameture@gmail.com>
2017-02-28 16:07:50 +0100

    importing bootstrap

--- camper-leaderboard/src/app/app.component.ts
+++ camper-leaderboard/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { isRegExp } from 'util';
+import { Component, OnInit } from '@angular/core';
+
+import { CampersService }    from './campers.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:  ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private campers: JSON;
+  private isRecent = true;
+
+  constructor(private campersService: CampersService) { }
+
+  ngOnInit(): void {
+    this.getRecentCampers();
+  }
+
+  private getRecentCampers() {
+    this.campersService.getRecentCampers()
+      .then(response => this.campers = response);
+    this.isRecent = true;
+  }
+
+  private getAlltimeCampers() {
+    this.campersService.getAlltimeCampers()
+      .then(response => this.campers = response);
+    this.isRecent = false;
+  }
 }
