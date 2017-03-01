bfab834606a19a1ca192896f50fb1d0697df94dd
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-02 00:16:17 +0100

    updating root module

--- recipe-box/src/app/app.module.ts
+++ recipe-box/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { RecipeDetailComponent } from './recipe-detail.component';
+import { AppComponent }              from './app.component';
+import { RecipeDetailComponent }     from './recipe-detail.component';
+import { EditRecipeFormComponent }   from './edit-recipe-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    EditRecipeFormComponent
   ],
   imports: [
     BrowserModule,
