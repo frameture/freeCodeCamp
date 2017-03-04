91532e190747f50acd4c3c058d02a336b186dff5
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-04 22:42:22 +0100

    updating root module

--- game-of-life/src/app/app.module.ts
+++ game-of-life/src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { GameService }            from './game.service';
+import { GridComponent }          from './grid/grid.component';
+import { StateControlsComponent } from './state-controls/state-controls.component';
+import { SettingsComponent }      from './settings/settings.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    StateControlsComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ GameService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
