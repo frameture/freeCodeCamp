e542c4ae3fd7cd5b62978524c34d018b7a4a40e0
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-05 16:34:11 +0100

    defining  the grids of the board

---
+++ roguelike-game/src/app/game/GRIDS.ts
@@ -0,0 +1,12 @@
+export const GRIDS = [
+  [
+    'ooooooooooooooooooooooooooooo',
+    'ooooo h                  oooo',
+    'ooooo         p         w ooo',
+    'ooooo                     ooo',
+    'ooooo  e    oooo      e   ooo',
+    'ooooo       oooo e     h  ooo',
+    'oooooooooooooooo          ooo',
+    'ooooooooooooooooooooooooooooo'
+  ]
+];
