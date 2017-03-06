6ddaccaa28a4bbda2571a8729f79dfbb19082007
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-06 14:52:29 +0100

    updating root module

--- roguelike-game/src/app/app.module.ts
+++ roguelike-game/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { GameInteractionService } from './game-interaction.service';
 import { GameService } from './game.service';
 import { InfoComponent } from './info/info.component';
 import { BoardComponent } from './board/board.component';
@@ -20,7 +21,10 @@ import { BoardComponent } from './board/board.component';
     FormsModule,
     HttpModule
   ],
-  providers: [ GameService ],
+  providers: [
+    GameInteractionService,
+    GameService
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
