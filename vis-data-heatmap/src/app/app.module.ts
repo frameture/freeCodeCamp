06663f864be9440014551e6c5779630bec7bff50
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-22 11:15:59 +0100

    initial commit

---
+++ vis-data-heatmap/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HeatMapComponent } from './heat-map/heat-map.component';
+import { TooltipComponent } from './tooltip/tooltip.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeatMapComponent,
+    TooltipComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
