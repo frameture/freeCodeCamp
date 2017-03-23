9363d22c7d782fa694cbe8e01ef51ce53a9eff26
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-23 11:03:18 +0100

    graph-holding component

---
+++ vis-data-force-directed-graph/src/app/graph/force-directed-graph/force-directed-graph.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Country, Link } from 'app/classes';
+import { DataService } from 'app/data.service';
+import { GraphService } from '../graph.service';
+
+@Component({
+  selector: 'app-force-directed-graph',
+  templateUrl: './force-directed-graph.component.html',
+  styleUrls: [ './force-directed-graph.component.css' ]
+})
+export class ForceDirectedGraphComponent implements OnInit {
+
+  private svgSelector = '.graph';
+  private height = 500;
+  private width = 600;
+  private countries: Country[];
+  private links: Link[];
+
+  constructor(
+    private dataService: DataService,
+    private graphService: GraphService
+  ) { }
+
+  ngOnInit() {
+    this.dataService.getData()
+      .subscribe((data) => {
+        this.extractData(data);
+        this.createGraph();
+      });
+  }
+
+  private createGraph(): void {
+    this.graphService.createGraph(
+      this.svgSelector,
+      this.width,
+      this.height,
+      this.countries,
+      this.links
+    );
+  }
+
+  private extractData(data: any): void {
+    this.countries = data.nodes;
+    this.links = data.links;
+  }
+
+}
