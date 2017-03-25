5888635dad834ec44ecd8cb8bdd1cd4a406a8766
Bartlomiej Laskowski <frameture@gmail.com>
2017-03-25 19:09:23 +0100

    fetching also map data

--- vis-data-globe/src/app/data.service.ts
+++ vis-data-globe/src/app/data.service.ts
@@ -7,12 +7,18 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  private url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
+  private readonly MAP_URL = 'https://d3js.org/world-50m.v1.json';
+  private readonly METEORITE_URL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
 
   constructor(private http: Http) { }
 
-  getData(): Observable<JSON> {
-    return this.http.get(this.url)
+  getMeteoriteData(): Observable<JSON> {
+    return this.http.get(this.METEORITE_URL)
+      .map(response => this.extractData(response));
+  }
+
+  getMapData(): Observable<JSON> {
+    return this.http.get(this.MAP_URL)
       .map(response => this.extractData(response));
   }
 
