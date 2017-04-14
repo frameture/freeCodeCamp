b3793e428d565c26f5b5d807ae14e524851f112b
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-14 20:37:41 +0200

    handling requests

---
+++ full-stack/timestamp-microservice/back-end/app.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+
+let app = express();
+app.use(logger('dev'));
+app.use(cors());
+
+app.get('/:date', requestHandler);
+app.listen('8080');
+
+function requestHandler(req: express.Request, res): void {
+  let param = req.params[ 'date' ];
+  if (!param) {
+    return res.json({ 'unix': null, 'natural': null });
+  }
+
+  let date;
+  if (!isNaN(+param)) {
+    date = new Date(+param);
+  } else {
+    date = new Date(param);
+  }
+
+  res.json({ 'unix': date.valueOf(), 'natural': date.toDateString() });
+}
