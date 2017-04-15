420a54b4755ad5ab9919d1b70e949309afe79e4c
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-15 13:53:21 +0200

    playing with AOT

--- full-stack/timestamp-microservice/front-end/src/main.ts
+++ full-stack/timestamp-microservice/front-end/src/main.ts
@@ -1,11 +1,10 @@
+import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { platformBrowser } from '@angular/platform-browser';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
