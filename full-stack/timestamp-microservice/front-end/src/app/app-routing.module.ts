6dcb7424badae37fc30b58dd48fb042c0c8421c1
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-15 13:53:44 +0200

    deployment changes

--- full-stack/timestamp-microservice/front-end/src/app/app-routing.module.ts
+++ full-stack/timestamp-microservice/front-end/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TimestampComponent } from 'app/timestamp/timestamp.component';
-import { HomeComponent } from 'app/home/home.component';
+import { TimestampComponent } from './timestamp/timestamp.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: ':date', component: TimestampComponent },
