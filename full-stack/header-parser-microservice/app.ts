4b5717dc1ee47845cf0024ce940b3d4c6f210493
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-15 16:01:06 +0200

    parsing headers

--- full-stack/header-parser-microservice/app.ts
+++ full-stack/header-parser-microservice/app.ts
@@ -8,6 +8,23 @@ app.get('/', requestHandler);
 app.listen(process.env.PORT || 8080);
 
 function requestHandler(req: express.Request, res): void {
-  console.log('headers', req.headers);
-  res.json({ message: 'not yet' });
+  const ip = getIp(req);
+  const os = getOs(req);
+  const language = getLang(req);
+
+  res.json({ ip, language, os });
+}
+
+function getIp(req): string {
+  return req.ip.substring(req.ip.lastIndexOf(':') + 1);
+}
+
+function getOs(req): string {
+  const ua = req.headers[ 'user-agent' ];
+  return ua.substring(ua.indexOf('(') + 1, ua.lastIndexOf(')'));
+}
+
+function getLang(req): string {
+  const al = req.headers[ 'accept-language' ];
+  return al.substring(0, al.indexOf(','));
 }
