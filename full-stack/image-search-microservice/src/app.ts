853c0f8bc9f6fd907e877d24a913aeb55084c7c3
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-16 19:46:34 +0200

    configuration

--- full-stack/image-search-microservice/src/app.ts
+++ full-stack/image-search-microservice/src/app.ts
@@ -1,13 +1,16 @@
+import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import * as logger from 'morgan';
 import { router } from './router';
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(logger('short'));
 app.use(router);
 mongoose.connect(process.env.DB_URL, connectionHandler);
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => console.log('Started.'));
 
 function connectionHandler(err): void {
   if (err) return console.error(err);
