d399659dff944fc225a21946f68c7df87044f14f
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-16 19:46:12 +0200

    home view

---
+++ full-stack/image-search-microservice/src/view.ts
@@ -0,0 +1,19 @@
+export const view = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <title></title>
+      <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+      <h1>Image Search Engine Microservice</h1>
+      <h4>To see recent searches visit:</h4> <p><code>https://image-search-microservice-app.herokuapp.com/recent</code> </p>
+      
+      <h4>Usage:</h4>
+      <code><p>https://image-search-microservice-app.com/search/some-term</p></code>
+      <code><p>https://image-search-microservice-app.com/search/germ?offset=4</p></code>
+    </body>
+  </html>
+`;
+
