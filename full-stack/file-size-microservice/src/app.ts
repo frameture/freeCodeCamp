6a8b89537e4be231ee912c4e0aff5d1957b85ed3
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-18 14:38:03 +0200

    configuration

--- full-stack/file-size-microservice/src/app.ts
+++ full-stack/file-size-microservice/src/app.ts
@@ -3,7 +3,7 @@ import * as logger from 'morgan';
 import { router } from './routes';
 
 const app = express();
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => console.log('Started.'));
 
 app.use(logger('short'));
 app.use(router);
