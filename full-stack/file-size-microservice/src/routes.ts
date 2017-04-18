044f3468ed26efef800f847ad47d06edda5d2571
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-18 14:38:32 +0200

    router

--- full-stack/file-size-microservice/src/routes.ts
+++ full-stack/file-size-microservice/src/routes.ts
@@ -7,3 +7,8 @@ router = express.Router();
 router.get('/', (req, res) => {
   res.send(view);
 });
+
+router.get('/file-size/:size', (req, res) => {
+  const size = req.params.size;
+  res.json({ size });
+});
