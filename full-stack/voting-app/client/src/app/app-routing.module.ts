c9be61f8495787775e5fb8ff5f98852a1254ae83
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-19 21:54:23 +0200

    app-routing with lazy loading

--- full-stack/voting-app/client/src/app/app-routing.module.ts
+++ full-stack/voting-app/client/src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, PreloadingStrategy } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from "app/components/home/home.component";
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
-  }
+    path: 'sign-up',
+    loadChildren: 'app/sign-up/sign-up.module.ts#SignUpModule'
+  },
+  {
+    path: 'login',
+    loadChildren: 'app/login/login.module.ts#LoginModule'
+  },
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(
+    routes,
+    { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
