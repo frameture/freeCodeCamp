3b1c813286e5a469b74016af4f8cfc9129c59940
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-19 22:01:18 +0200

    updating

--- full-stack/voting-app/client/src/app/app.module.ts
+++ full-stack/voting-app/client/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +21,6 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
