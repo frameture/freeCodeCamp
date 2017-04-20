d7ccec84c4f239c4fbd62aba143d00a4109d0eeb
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-20 21:45:37 +0200

    sign-up service

--- full-stack/voting-app/client/src/app/services/sign-up.service.ts
+++ full-stack/voting-app/client/src/app/services/sign-up.service.ts
@@ -1,8 +1,15 @@
+import { User } from '../models/user';
+import { Observable } from 'rxjs/Rx';
+import { BackendService } from './backend.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SignUpService {
 
-  constructor() { }
+  constructor(private be: BackendService) { }
+
+  signUp(user: User): Observable<any> {
+    return this.be.register(user);
+  }
 
 }
