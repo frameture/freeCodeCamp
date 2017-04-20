9b740089156d50448507b3b183d08d6e6603e082
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-20 21:47:59 +0200

    logging in through the user service

--- full-stack/voting-app/client/src/app/features/login/login.component.ts
+++ full-stack/voting-app/client/src/app/features/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -27,6 +28,7 @@ export class LoginComponent implements OnInit {
 
   onLogout(): void {
     this.userService.logout();
+    this.createTitle();
   }
 
   onNavigateTo(path: string): void {
@@ -38,9 +40,15 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log('submit');
-    this.resetForm();
-    this.router.navigate([ '/' ]);
+    if (this.userService.isLoggedIn()) { return; }
+
+    const user = this.getControls();
+    this.userService
+      .login(user)
+      .subscribe((res) => {
+        this.resetForm();
+        this.onNavigateTo('/');
+      });
   }
 
   private createForm(): void {
@@ -54,6 +62,15 @@ export class LoginComponent implements OnInit {
     this.title = this.isLoggedIn()
       ? 'You are logged in.'
       : 'Login in to the Voting App';
+  }
+
+
+  private getControls(): User {
+    const user = new User(
+      this.form.get('username').value,
+      this.form.get('password').value,
+    );
+    return user;
   }
 
   private isLoggedIn(): boolean {
