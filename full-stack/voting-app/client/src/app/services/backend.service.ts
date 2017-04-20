045808aa8d477c83df1babb499ad85ef7f73356e
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-20 21:45:54 +0200

    talking to the server

--- full-stack/voting-app/client/src/app/services/backend.service.ts
+++ full-stack/voting-app/client/src/app/services/backend.service.ts
@@ -1,8 +1,22 @@
+import { Observable } from 'rxjs/Rx';
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+
+import { User } from '../models/user';
 
 @Injectable()
 export class BackendService {
 
-  constructor() { }
+  private readonly URL = 'http://localhost:8080/server';
+
+  constructor(private http: Http) { }
+
+  register(data: User): Observable<any> {
+    return this.http.post(this.URL + '/sign-up', { data });
+  }
+
+  login(data: User): Observable<any> {
+    return this.http.post(this.URL + '/login', { data });
+  }
 
 }
