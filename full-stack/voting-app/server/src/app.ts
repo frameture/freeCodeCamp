6af4c1db9ecd4c13633dce7cf5416c1ffba85a22
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-20 17:29:53 +0200

    configuration

---
+++ full-stack/voting-app/server/src/app.ts
@@ -0,0 +1,19 @@
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as mongoose from 'mongoose';
+import { router } from './routes'
+
+import { DB_URI } from '../.env';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger('dev'));
+app.use(router);
+
+mongoose.connect(process.env.DB_URI || DB_URI, console.error);
+app.listen(process.env.PORT || 8080, () => console.log('Started.'));
