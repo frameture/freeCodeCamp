20546f85369c198af17135825aeeb070d6115497
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-20 11:23:06 +0200

    home component

--- full-stack/voting-app/client/src/app/components/home/home.component.ts
+++ full-stack/voting-app/client/src/app/components/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: [ './home.component.scss' ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  info: string;
 
-  ngOnInit() {
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  onNavigateTo(path: string): void {
+    this.router.navigate([ path ]);
   }
+
+  private isLoggedIn(): boolean {
+    return this.userService.isLoggedIn();
+  }
+
 
 }
