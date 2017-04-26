0420ef137561f0cbbf4576462b4f17668d0b9405
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-26 15:06:42 +0200

    user can tag himeslf going or not to certain venues

--- full-stack/nightlife-coordination/client/src/app/features/bars/bars.component.ts
+++ full-stack/nightlife-coordination/client/src/app/features/bars/bars.component.ts
@@ -1,8 +1,9 @@
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
 
 import { BarsService } from '../../services/bars.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-bars',
@@ -12,16 +13,52 @@ import { BarsService } from '../../services/bars.service';
 export class BarsComponent implements OnInit {
 
   bars;
+  going;
 
   constructor(
     private route: ActivatedRoute,
-    private barsService: BarsService
+    private router: Router,
+    private barsService: BarsService,
+    private userService: UserService
   ) { }
+
+  doIGo(id: string): string {
+    const clientId = this.userService.getProfile();
+    for (let i = 0; i < this.going.length; i++) {
+      const venue = this.going[ i ];
+      if (venue.venueId !== id) { continue; }
+      if (venue.going && venue.going.indexOf(clientId) >= 0) {
+        return 'yes';
+      }
+    }
+    return 'no';
+  }
+
+  howManyGoing(id: string): number {
+    for (let i = 0; i < this.going.length; i++) {
+      const venue = this.going[ i ];
+      if (venue.venueId !== id) { continue; }
+      return venue.going.length || 0;
+    }
+    return 0;
+  }
 
   ngOnInit() {
     this.route.params
-      .switchMap(params => this.barsService.getBars(params['location']))
-      .subscribe(res => this.bars = res);
+      .switchMap(params => this.barsService.getBars(params[ 'location' ]))
+      .subscribe((res) => {
+        this.going = res.going;
+        this.bars = res.venues;
+      });
   }
+
+  onGo(id: string): void {
+    console.log('onGo', id);
+    this.barsService
+      .setBar(id)
+      .subscribe(res =>
+        this.router.navigate([ '/' ]) );
+  }
+
 
 }
--- full-stack/nightlife-coordination/client/src/app/services/bars.service.ts
+++ full-stack/nightlife-coordination/client/src/app/services/bars.service.ts
@@ -1,3 +1,4 @@
+import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
@@ -7,11 +8,20 @@ import { BackendService } from './backend.service';
 @Injectable()
 export class BarsService {
 
-  constructor(private be: BackendService) { }
+  constructor(
+    private be: BackendService,
+    private us: UserService
+  ) { }
 
   getBars(location: string): Observable<any> {
     return this.be
       .getBars(location)
+      .map(res => res.json());
+  }
+
+  setBar(venueId: string): Observable<any> {
+    return this.be
+      .setBar(venueId, this.us.getProfile())
       .map(res => res.json());
   }
 }
