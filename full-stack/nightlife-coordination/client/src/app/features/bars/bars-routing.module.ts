dd112f1626ab6f7fede1b554b8e7366120fb089e
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-26 15:05:20 +0200

    guarding the bars search

--- full-stack/nightlife-coordination/client/src/app/features/bars/bars-routing.module.ts
+++ full-stack/nightlife-coordination/client/src/app/features/bars/bars-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../../services/auth-guard.service';
 import { BarsComponent } from './bars.component';
 
 const routes: Routes = [
   {
-    path: '', children: [
-      { path: ':location', component: BarsComponent }
+    path: '', canActivate: [ AuthGuard ], children: [
+      {
+        path: ':location',
+        component: BarsComponent
+      }
     ]
   },
 ];
