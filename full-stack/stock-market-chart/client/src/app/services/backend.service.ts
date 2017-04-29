0942589ff5ab2d0cb89ad168843d2c2c2a1ce97f
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-29 17:03:09 +0200

    extracting data from the responses

--- full-stack/stock-market-chart/client/src/app/services/backend.service.ts
+++ full-stack/stock-market-chart/client/src/app/services/backend.service.ts
@@ -13,19 +13,35 @@ export class BackendService {
   constructor(private http: Http) { }
 
   getStock(query: string): Observable<any> {
-    return this.http.get(`${ this.URL }/stock-data/${ query }`);
+    return this.http
+      .get(`${ this.URL }/stock-data/${ query }`)
+      .map((res: any) => {
+        let body = res._body;
+        body = body.substring(0, body.lastIndexOf('undefined'));
+        return JSON.parse(body);
+      });
   }
 
   getStocks(): Observable<any> {
-    return this.http.get(`${ this.URL }/stocks`);
+    return this.http
+      .get(`${ this.URL }/stocks`)
+      .map(res => this.extractData(res));
   }
 
   addStock(stock: string): Observable<any> {
-    return this.http.get(`${ this.URL }/stock-add/${ stock }`);
+    return this.http
+      .get(`${ this.URL }/stock-add/${ stock }`)
+      .map(res => this.extractData(res));
   }
 
   removeStock(stock: string): Observable<any> {
-    return this.http.get(`${ this.URL }/stock-remove/${ stock }`);
+    return this.http
+      .get(`${ this.URL }/stock-remove/${ stock }`)
+      .map(res => this.extractData(res));
+  }
+
+  private extractData(res) {
+    return res.json();
   }
 
 }
