ee31762fa4a065f6f5460f184167e0a63e7c88a5
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-29 16:59:40 +0200

    SoscketService - resonsible for Socket.IO communication

--- full-stack/stock-market-chart/client/src/app/services/socket.service.ts
+++ full-stack/stock-market-chart/client/src/app/services/socket.service.ts
@@ -5,13 +5,39 @@ import * as socket from 'socket.io-client';
 
 @Injectable()
 export class SocketService {
-  
-  private io;
 
-  constructor(private stockService: StockService) { }
-  
-  main(): void {
-    this.io = socket.connect('http://localhost:8181');
-    
+  private io: SocketIOClient.Socket;
+
+  constructor(private stockService: StockService) {
+    this.setSocket();
   }
+
+  addStock(stock: string): void {
+    this.stockService.addStock(stock, () => this.broadcastAdd(stock));
+  }
+
+  removeStock(stock: string): void {
+    this.stockService.removeStock(stock, () => this.broadcastRemove(stock));
+  }
+
+  broadcastAdd(stock: string, ): void {
+    this.io.emit('add', stock);
+  }
+
+  broadcastRemove(stock: string): void {
+    this.io.emit('remove', stock);
+  }
+
+  private setSocket(): void {
+    this.io = socket('http://localhost:8181');
+
+    this.io.on('remove', (stock) => {
+      this.stockService.updateData(stock, 'remove');
+    });
+
+    this.io.on('add', (stock) => {
+      this.stockService.updateData(stock, 'add');
+    });
+  }
+
 }
