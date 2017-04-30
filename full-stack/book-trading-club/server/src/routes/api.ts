a9629ea9000df9083ed7f2f4107158c40578e3ef
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 09:54:25 +0200

    add-book and user-update routes

--- full-stack/book-trading-club/server/src/routes/api.ts
+++ full-stack/book-trading-club/server/src/routes/api.ts
@@ -1,8 +1,40 @@
 import { Router } from 'express';
 
+import { Book } from '../models/book';
 import { User } from '../models/user';
 
 export const api = Router();
+
+api.post('/add-book', (req, res, next) => {
+  const data = req.body.data;
+
+  new Book({
+    name: data.name,
+    owner: data.owner
+  }).save((err, newBook) => {
+    if (err) { return next(err); }
+    
+    User.findOne({ username: data.owner }, (err, doc: any) => {
+      if (err) { return next(err); }
+      if (!doc) { return next(new Error('No such user')); }
+      
+      doc.books.push(newBook._id);
+      doc.save((err) => {
+        if (err) { return next(err); }
+        res.end();
+      })
+    });
+  })
+});
+
+api.post('/user-update', (req, res, next) => {
+  const data = req.body.data;
+
+  User.findByIdAndUpdate(data._id, data, (err, doc) => {
+    if (err) { return next(err); }
+    res.json(doc);
+  });
+});
 
 api.post('/login', (req, res, next) => {
   const data = req.body.data;
