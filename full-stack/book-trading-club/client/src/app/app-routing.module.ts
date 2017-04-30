7ec9c06526ac7e3e5aa2467ad5102dd34d71955b
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 23:26:37 +0200

    root routing

--- full-stack/book-trading-club/client/src/app/app-routing.module.ts
+++ full-stack/book-trading-club/client/src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
-  }
+    path: 'auth',
+    loadChildren: 'app/features/auth/auth.module#AuthModule'
+  },
+  {
+    path: 'user',
+    loadChildren: 'app/features/user/user.module#UserModule'
+  },
+  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- full-stack/book-trading-club/client/src/app/app.module.ts
+++ full-stack/book-trading-club/client/src/app/app.module.ts
@@ -3,15 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AuthGuard } from './services/auth-guard.service';
 import { AuthService } from './services/auth.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BackendService } from './services/backend.service';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -20,6 +25,7 @@ import { UserService } from './services/user.service';
     AppRoutingModule
   ],
   providers: [
+    AuthGuard,
     AuthService,
     BackendService,
     UserService
