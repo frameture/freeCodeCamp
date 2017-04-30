dce16cef6ee1c72fa05dbc185d257ed2d383895e
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 09:24:42 +0200

    /login route

--- full-stack/book-trading-club/server/src/routes/api.ts
+++ full-stack/book-trading-club/server/src/routes/api.ts
@@ -4,19 +4,33 @@ import { User } from '../models/user';
 
 export const api = Router();
 
-api.post('/sign-up', (req, res) => {
+api.post('/login', (req, res, next) => {
+  const data = req.body.data;
+
+  User.findOne({ username: data.username }, (err, doc: any) => {
+    if (err) { return next(err); }
+    if (!doc) { return next(new Error('No such user')); }
+
+    doc.checkPassword(data.password, (err, isMatch) => {
+      if (!isMatch) next({ message: 'Wrong password' });
+      res.end();
+    });
+  })
+});
+
+api.post('/sign-up', (req, res, next) => {
   const data = req.body.data;
 
   User.findOne({ username: data.username }, (err, doc) => {
-    if (err) { return console.error(err); }
-    if (doc) { return res.json({ message: 'Username taken' }); }
+    if (err) { return next(err); }
+    if (doc) { return next({ message: 'Username taken' }); }
 
     new User({
       username: data.username,
       password: data.password
     }).save((err) => {
-      if (err) { return console.error(err); }
-      res.json({ success: true });
+      if (err) { return next(err); }
+      res.end();
     });
   });
 });
