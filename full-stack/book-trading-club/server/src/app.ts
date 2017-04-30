fc9fef6f978f9421310540f7a71ed03730dfa5ac
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 12:25:50 +0200

    employing error handling middleware

--- full-stack/book-trading-club/server/src/app.ts
+++ full-stack/book-trading-club/server/src/app.ts
@@ -2,9 +2,10 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as logger from 'morgan';
 import * as path from 'path';
 
-import { DB_URI } from '../env';
+import { DB_URI } from './env';
 import { api } from './routes/api';
 import { index } from './routes/index';
 
@@ -13,10 +14,18 @@ export const publicPath = path.resolve(__dirname, 'public');
 const app = express();
 
 app.use(cors()) // TODO
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api', api);
 app.use(index);
+
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log('ERROR HANDLING MIDDLEWARE \n\n\n ', err);
+    res.json(err);
+  }
+});
 
 mongoose.connect(DB_URI, () => console.log('Connected to the DB'));
 app.listen(process.env.PORT || 8080, () => console.log('Started on 8080:'));
