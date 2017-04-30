1d33954eeac37f7a57fd6fecefa31eeb297c85f2
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 23:27:38 +0200

    navigation

--- full-stack/book-trading-club/client/src/app/app.component.ts
+++ full-stack/book-trading-club/client/src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { AuthService } from './services/auth.service';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  constructor(private authService: AuthService) { }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
 }
