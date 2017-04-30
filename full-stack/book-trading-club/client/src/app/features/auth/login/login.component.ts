2964442bd3de8765ddfc1a08cf33b8b08c3f4195
Bartlomiej Laskowski <frameture@gmail.com>
2017-04-30 23:23:52 +0200

    login form

--- full-stack/book-trading-club/client/src/app/features/auth/login/login.component.ts
+++ full-stack/book-trading-club/client/src/app/features/auth/login/login.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: [ './login.component.scss' ]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  errorInfo: string;
+  form: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  onSubmit(): void {
+    console.log('submit');
+    this.authService
+      .login(this.getControl('username'), this.getControl('password'))
+      .subscribe((res) => {
+        console.log(res);
+        if (res.message) { return this.errorInfo = res.message; }
+        this.router.navigate([ '/' ]);
+      });
+  }
+
+  private createForm(): void {
+    this.form = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
+  }
+
+  private getControl(control: string): string {
+    return this.form.get(control).value;
   }
 
 }
