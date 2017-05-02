01658a7f8a14b9ac3834514d89a1350d0883aacc
Bartlomiej Laskowski <frameture@gmail.com>
2017-05-02 11:03:12 +0200

    book-related requests

--- full-stack/book-trading-club/client/src/app/services/backend.service.ts
+++ full-stack/book-trading-club/client/src/app/services/backend.service.ts
@@ -11,15 +11,41 @@ export class BackendService {
 
   constructor(private http: Http) { }
 
+  acceptRequest(reqId, username): Observable<any> {
+    const data = { reqId, username };
+    return this.http
+      .post(this.URL + '/accept-request', { data })
+      .map(res => this.extractData(res));
+  }
+
   addBook(data): Observable<any> {
     return this.http
       .post(this.URL + '/add-book', { data })
       .map(res => this.extractData(res));
   }
 
+  addRequest(bookId: string, username: string): Observable<any> {
+    const data = { bookId, username };
+    return this.http
+      .post(this.URL + '/add-request', { data })
+      .map(res => this.extractData(res));
+  }
+
   getProfile(id: string): Observable<any> {
     return this.http
       .get(this.URL + '/user/' + id)
+      .map(res => this.extractData(res));
+  }
+
+  getBook(bookId: string): Observable<any> {
+    return this.http
+      .get(this.URL + '/book/' + bookId)
+      .map(res => this.extractData(res));
+  }
+
+  getBooks(): Observable<any> {
+    return this.http
+      .get(this.URL + '/books')
       .map(res => this.extractData(res));
   }
 
@@ -38,6 +64,13 @@ export class BackendService {
   update(data): Observable<any> {
     return this.http
       .post(this.URL + '/update-profile', { data })
+      .map(res => this.extractData(res));
+  }
+
+  removeBook(username: string, bookId: string): Observable<any> {
+    const data = { username, bookId };
+    return this.http
+      .post(this.URL + '/remove-book', { data })
       .map(res => this.extractData(res));
   }
 
