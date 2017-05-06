e598b56024ef807232e63f54fb6c55f0c7a15fdc
Bartlomiej Laskowski <frameture@gmail.com>
2017-05-06 23:10:07 +0200

    update

--- full-stack/pinterest-clone/server/src/app.ts
+++ full-stack/pinterest-clone/server/src/app.ts
@@ -1,12 +1,15 @@
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 import * as logger from 'morgan';
 import * as path from 'path';
 
 import { DB_URI } from './env'; // TODO
-import { apiRouter } from './routes/api';
+import { apiUserRouter } from './routes/api-user';
+import { apiAuthRouter } from './routes/api-auth';
 import { indexRouter } from './routes/index';
+import { errorHandler } from './error-handler';
 
 export const publicPath = path.resolve(__dirname, 'public');
 
@@ -16,8 +19,10 @@ app.use(cors()); // TODO
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/api', apiRouter);
+app.use('/api', apiUserRouter);
+app.use('/api', apiAuthRouter);
 app.use(indexRouter);
+app.use(errorHandler);
 
-// app.connect(process.env.DB_URI || DB_URI, () => console.log('DB-ected'));
+mongoose.connect(process.env.DB_URI || DB_URI, () => console.log('DB-ected'));
 app.listen(process.env.PORT || 8080, () => console.log('Started'));
