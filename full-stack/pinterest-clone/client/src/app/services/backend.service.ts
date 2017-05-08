8dae8b7eca1904005d4a7fc912a4fff72d80a6d8
Bartlomiej Laskowski <frameture@gmail.com>
2017-05-08 16:46:28 +0200

    configuration

--- full-stack/pinterest-clone/client/src/app/services/backend.service.ts
+++ full-stack/pinterest-clone/client/src/app/services/backend.service.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 export class BackendService {
 
-  private readonly URL = 'http://localhost:8080/api'; // TODO
+  private readonly URL = '/api';
 
   constructor(private http: Http) { }
 
